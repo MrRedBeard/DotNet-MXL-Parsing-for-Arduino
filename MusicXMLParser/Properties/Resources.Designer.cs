4751b6a5eea6111745c4f009a6cefa24065e628c
MrRedBeard <britton.scritchfield@gmail.com>
2022-10-15 16:39:25 -0500

    Prep for release
    Logo added

--- MusicXMLParser/Properties/Resources.Designer.cs
+++ MusicXMLParser/Properties/Resources.Designer.cs
@@ -79,5 +79,25 @@ namespace MusicXMLParser.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap logo {
+            get {
+                object obj = ResourceManager.GetObject("logo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon logo1 {
+            get {
+                object obj = ResourceManager.GetObject("logo1", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
